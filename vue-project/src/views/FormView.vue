0bdcf6ca68c979cbc24049d07972d2b4df075e79
yousaf anwar <yousafanwar74@gmail.com>
2024-07-09 16:15:44 +0500

    Form View Component Implemented

---
+++ vue-project/src/views/FormView.vue
@@ -0,0 +1,36 @@
+<template>
+    <form>
+         <div class="col-md-4">
+            <label for="validationCustom01" class="form-label">User Name</label>
+            <input type="text" v-model="userName" class="form-control" id="validationCustom01" required>
+            <div class="valid-feedback">
+            </div>
+        </div>
+  <div class="col-md-4">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" v-model="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="col-md-4">
+    <label for="exampleInputEmail1" class="form-label">Phone</label>
+    <input type="tel" v-model="phone" class="form-control" maxlength="10">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+        userName: '',
+        email: '',
+        phone: ''
+    }
+  },
+  methods:{
+
+  }  
+}
+</script>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,8 +1,75 @@
 <template>
-  
+<div>
+  <!-- <h1> {{abc}} </h1> 
+  <button @click="fetchData">Push me to get data</button> -->
+  <ul>
+    <li v-for="event in events" :key="event.id" class="eventContainer">
+      <h1>{{event.name}}</h1>
+        <p>{{event.date}}</p>
+        <p>{{event.location}}</p>
+        <RouterLink class="btn" to="./form">Register</RouterLink>
+    </li>
+  </ul>
+</div>
 </template>
 
 
-<script setup>
+<script>
+    import axios from 'axios';
+    import { RouterLink } from 'vue-router';
+  export default{
+
+    data(){
+      return{
+        abc: "data",
+        events: [
+          {id: 1, name: 'Party', date: new Date().getDate(), location: 'Lahore'},
+          {id: 2, name: 'Office Meeting', date: new Date().getDate(), location: 'Lahore'},
+          {id: 3, name: 'Just chill', date: new Date().getDate(), location: 'Muree'},
+          ]
+      }
+    },
+    mounted(){
+      // this.fetchData;
+      },
+    methods:{
+      async fetchData(){
+        // api key: MKSTDUMZDEQJUDF6X5IP
+  const response = await axios.get('https://www.eventbriteapi.com/v3/events/search/', {
+          headers: {
+            Authorization: `Bearer MKSTDUMZDEQJUDF6X5IP`
+          },
+   params: {
+            q: 'conference', // Example query parameter
+            'location.address': 'New York', // Correctly use quotes for keys with special characters like '.'
+            sort_by: 'date',
+            page: 1,
+            page_size: 10 // Example pagination
+          }
+        });
+          // const result = await response;
+          // console.log(result);
+      },
+    }
+  }
 </script>
 
+<style scoped>
+  li{
+    list-style: none;
+  }
+  .eventContainer{
+    border: solid hsl(30, 15%, 87%);;
+    border-radius: 10px;
+    background-color: hsl(30, 3%, 87%);
+    margin: 10px;
+    padding: 10px;
+  }
+  .btn{
+    background-color: white;
+  }
+  .btn:hover{
+    background-color: black;
+    color: white;
+  }
+</style>
